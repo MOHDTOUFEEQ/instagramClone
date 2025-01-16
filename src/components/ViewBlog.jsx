0c40b53f36f035f1aa365317b3bfecd3045707f3
GroupResearchProject <mohdtoufeeq1447@gmail.com>
2025-01-16 18:09:08 +0000

    fixed edit/delete from the profile section

--- src/components/ViewBlog.jsx
+++ src/components/ViewBlog.jsx
@@ -1,9 +1,8 @@
 import  { useEffect, useState } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import service from '../appwrite/config';
 function ViewBlog() {
     const { id } = useParams();
-    const navigate = useNavigate()
     const [post, setPost] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
@@ -13,7 +12,6 @@ function ViewBlog() {
         service.getPost(id)
             .then((curr_post) => {
                 setPost(curr_post);
-                console.log(curr_post);
                 setLoading(false);
             })
             .catch((err) => {
@@ -22,9 +20,7 @@ function ViewBlog() {
             });
     }, [id]);
     
-    const handleDelete = () => {
-        service.deletePost(id).then(() => service.deleteFile(post.featuredImage)).then(()=>navigate("/"));
-    };
+   
 
     if (loading) {
         return <p className="text-center mt-8">Loading...</p>;
@@ -45,14 +41,7 @@ function ViewBlog() {
             <p className="text-gray-700 mb-4">{post.content}</p>
             {/* Render other post details here */}
             
-            <div className="flex justify-end">
-                <Link to={`/Edit/${id}`} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2 rounded">
-                    Edit
-                </Link>
-                <button onClick={handleDelete} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                    Delete
-                </button>
-            </div>
+            
         </div>
     );
 }
